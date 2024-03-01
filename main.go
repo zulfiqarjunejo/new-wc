fe93bdc70a158ceecc8ec70f8c252138f988657f
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2024-03-01 13:15:07 +0100

    step 1: make it work

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello, new-wc-go")
+	_ = flag.Bool("c", false, "count number of bytes")
+
+	flag.Parse()
+
+	filename := flag.Arg(0)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Failed to open %s: %s\n", filename, err.Error())
+	}
+	defer file.Close()
+
+	content := make([]byte, 9999999)
+	n, err := file.Read(content)
+
+	fmt.Printf("%d %s\n", n, filename)
 }
