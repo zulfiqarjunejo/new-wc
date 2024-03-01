418e1c96df7731134aa4b16408e51f693f9b8c5e
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2024-03-01 19:23:58 +0100

    step 5: make it work

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"strings"
 )
 
+type CountCollection struct {
+	Bytes      int
+	Characters int
+	Lines      int
+	Words      int
+}
+
 func main() {
 	c := flag.Bool("c", false, "count number of bytes")
 	l := flag.Bool("l", false, "count number of lines")
@@ -22,6 +29,22 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to read %s: %s\n", filename, err.Error())
 	}
+
+	cc := CountCollection{}
+
+	cc.Bytes = len(content)
+	cc.Characters = len(content)
+
+	lines := strings.Split(string(content), "\n")
+	cc.Lines = len(lines) - 1
+
+	var wordCount int
+	for _, line := range lines {
+		wordsInLine := strings.Fields(line)
+		wordCount += len(wordsInLine)
+	}
+
+	cc.Words = wordCount
 
 	if *c {
 		fmt.Printf("  %d %s\n", len(content), filename)
@@ -38,5 +61,7 @@ func main() {
 		fmt.Printf("  %d %s\n", wordCount, filename)
 	} else if *m {
 		fmt.Printf("  %d %s\n", len(content), filename)
+	} else {
+		fmt.Printf("  %d %d %d %s\n", cc.Lines, cc.Words, cc.Bytes, filename)
 	}
 }
